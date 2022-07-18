861b1820e9f8845654071e45b7b2f39f36f29dd7
Naylson Costa <naylsonrj@gmail.com>
2022-07-18 17:08:09 -0300

    configurando api

--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -10,7 +10,7 @@ import { map, tap } from "rxjs/operators";
 })
 export class PokeApiService {
 
-  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=50';
+  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=100';
 
   constructor(
     private http: HttpClient
